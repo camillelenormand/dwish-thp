900468d91cc9e1dd0720b3fb2ba529041be6fcb5
Camille Lenormand <camille.lenormand02@gmail.com>
2023-06-09 12:48:10 +0200

    change cart icon

--- app/components/cart_button_component.rb
+++ app/components/cart_button_component.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class CartButtonComponent < ViewComponent::Base
-  def initialize(cart_size:, icon:, url: )
+  def initialize(cart_size:, icon:, url:, style: )
     @cart_size = 0
     @icon = icon
     @url = url
+    @style = style
   end
 end

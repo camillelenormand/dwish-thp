f5730837bdb1b23a2d8ff3b5837588b578364e2f
Camille Lenormand <camille.lenormand02@gmail.com>
2023-06-09 14:45:20 +0200

    add profile page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   add_flash_types :info, :error, :warning, :success
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :lastname, :email, :password)}
+
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :phone, :current_password)}
+  end
+
 end

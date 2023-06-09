81cb8431be58020515c884ea8d4927f5001d46c4
Camille Lenormand <camille.lenormand02@gmail.com>
2023-06-09 18:10:45 +0200

    fix bug create account

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,5 @@
 class ApplicationController < ActionController::Base
   add_flash_types :info, :error, :warning, :success
-  protect_from_forgery with: :exception
-
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :lastname, :email, :password)}

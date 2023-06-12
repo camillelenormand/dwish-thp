775a70981a17c362afd06c3d1350cd8d771661e5
Camille Lenormand <camille.lenormand02@gmail.com>
2023-06-12 12:11:21 +0200

    fix order table

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,6 @@ class Order < ApplicationRecord
   validates :user_id, presence: true
   validates :status, presence: true
   
-  belongs_to :cart ,index: { unique: true }, foreign_key: true
+  belongs_to :cart, foreign_key: true
   belongs_to :user
 end

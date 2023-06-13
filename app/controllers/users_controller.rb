fe69d345a31be7f00c65438f317edcc103ff451c
Camille Lenormand <camille.lenormand02@gmail.com>
2023-06-13 14:29:15 +0200

    fix flash styles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,8 +23,7 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if current_user.update(user_params)
-        flash[:notice] = "Votre profil a bien été mis à jour."
-        redirect_to root_path
+        format.html { redirect_to root_path, notice: "Votre profil a bien été mis à jour." }
       else
         format.html { render :edit, alert: "Votre profil n'a pas pu être mis à jour. Merci de réessayer." }
       end

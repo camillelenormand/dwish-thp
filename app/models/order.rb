fbbe8c0767e643623986ab9637540571af6d6620
Camille Lenormand <camille.lenormand02@gmail.com>
2023-06-15 14:51:48 +0200

    edit order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,8 @@
 class Order < ApplicationRecord
-  validates :user_id, presence: true
   validates :status, presence: true
-  enum status: { in_progress: 0, validated: 1, paid: 2, cancelled: 3, trashed: 4, refunded: 5 }
+  enum status: { pending: 0, paid: 1, cancelled: 2, refunded: 3}
   validates :amount, presence: true
   
-  belongs_to :cart, foreign_key: true
-  belongs_to :user
+  belongs_to :cart, presence: true
+  belongs_to :user, presence: true
 end

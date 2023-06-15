e4e31e3c9982ae772876f6f04cb94a51e05c6a97
Camille Lenormand <camille.lenormand02@gmail.com>
2023-06-15 13:55:03 +0200

    add status in model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,8 @@
 class Order < ApplicationRecord
   validates :user_id, presence: true
   validates :status, presence: true
+  enum status: { in_progress: 0, validated: 1, paid: 2, cancelled: 3, trashed: 4, refunded: 5 }
+  validates :amount, presence: true
   
   belongs_to :cart, foreign_key: true
   belongs_to :user

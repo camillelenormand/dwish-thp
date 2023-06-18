f8005999268c83b4194b8640efb1803a4ec39f64
Camille Lenormand <camille.lenormand02@gmail.com>
2023-06-18 21:57:09 +0200

    update mail with variables

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -10,7 +10,8 @@ class UserMailer < ApplicationMailer
   end
 
   def confirm_order_email(user)
-    user_id = user.id
+    user_id = @user.id
+    @order = Order.where(user_id: user_id).last
 
     url = f"https://dwish-staging.herokuapp.com/users/#{user_id}"
     return url

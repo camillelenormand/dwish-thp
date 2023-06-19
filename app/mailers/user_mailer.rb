df675e8ca3163002da73bb44e0f6f2801e2a56b1
Camille Lenormand <camille.lenormand02@gmail.com>
2023-06-19 11:40:20 +0200

    fix emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,13 +3,13 @@ class UserMailer < ApplicationMailer
 
   def welcome_email(user)
     @user = user
-
     @url = 'https://dwish-staging.herokuapp.com/users/sign_in'
-
     mail(to: @user.email, subject: 'Bienvenue chez Dwish !')
   end
 
   def confirm_order_email(user)
+    @user = user
+    @url = "https://dwish-staging.herokuapp.com/users/#{user.id}"
     user_id = @user.id
     @order = Order.where(user_id: user_id).last
 
@@ -18,5 +18,4 @@ class UserMailer < ApplicationMailer
 
     mail(to: @user.email, subject: 'Confirmation de votre commande')
   end
-
 end
--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,13 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
 
+  def welcome_email
+    UserMailer.welcome_email(User.first)
+  end
+
+  def confirm_order_email
+    @order = Order.first
+    UserMailer.confirm_order_email(@order.user)
+  end
+
 end

92202fea16ef317d1efe3339e6386faaa90ce821
Camille Lenormand <camille.lenormand02@gmail.com>
2023-06-20 14:35:31 +0200

    fix application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -32,12 +32,7 @@ module ApplicationHelper
   def navbar_cart_path
     @cart=Cart.find_by(user_id: current_user.id)
     
-    pp @cart
-    puts "@cart=Cart.find_by(user_id: current_user.id)  #{@cart}  user_id: current_user.id #{current_user.id} "
-    puts "@cart.id: #{@cart&.id} " 
-    
       if @cart.present? 
-        puts "#cart.present? #{@cart.present? }"
         my_cart_path=cart_path(@cart&.id)
       else
         my_cart_path=items_path

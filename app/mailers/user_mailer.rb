1d5bf4a253e4fa3a92ff95582a50cd2ab64692e5
Camille Lenormand <camille.lenormand02@gmail.com>
2023-06-21 15:32:34 +0200

    fix mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,14 +3,14 @@ class UserMailer < ApplicationMailer
 
   def welcome_email(user)
     @user = user
-    @url = 'https://dwish-staging.herokuapp.com/users/sign_in'
+    @url = 'https://dwish.herokuapp.com/users/sign_in'
     mail(to: @user.email, subject: 'Bienvenue chez Dwish !')
   end
 
   def confirm_order_email(user, order)
     @user = user
     @order = order
-    @url = "https://dwish-staging.herokuapp.com/orders/#{order.id}"
+    @url = "https://dwish.herokuapp.com/orders/#{order.id}"
 
     mail(to: @user.email, subject: 'Confirmation de votre commande')
   end
